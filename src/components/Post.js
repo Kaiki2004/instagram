9e1858dd92c53a8c774b239c748e822581d7d697
Kaiki <kaikiandrade64@gmail.com>
2025-07-25 18:54:28 -0300

    CREATED: Criação das funcionalidades do post (curtidas, salvar, outros)

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,13 +1,40 @@
+import { useState } from "react";
+
 export default function Post() {
     const posts = [
-        { imagem: "assets/img/gato-telefone.svg", imgUsuario: "assets/img/meowed.svg", usuario: "meowed" },
-        { imagem: "assets/img/dog.svg", imgUsuario: "assets/img/catanacomics.svg", usuario: "respondeai" },
-        { imagem: "assets/img/chibirdart.svg", imgUsuario: "assets/img/chibirdart.svg", usuario: "barked" },
+        { imagem: "assets/img/gato-telefone.svg", imgUsuario: "assets/img/meowed.svg", usuario: "meowed", curtidas: 101523 },
+        { imagem: "assets/img/dog.svg", imgUsuario: "assets/img/catanacomics.svg", usuario: "respondeai", curtidas: 101500 },
+        { imagem: "assets/img/chibirdart.svg", imgUsuario: "assets/img/chibirdart.svg", usuario: "barked", curtidas: 101400 },
     ];
 
-    function PostItem({ imagem, imgUsuario, usuario }) {
+    function PostItem({ imagem, imgUsuario, usuario, curtidasIniciais }) {
+        const [savePost, setSavePost] = useState('bookmark-outline');
+        const [like, setLike] = useState('heart-outline');
+        const [curtidas, setCurtidas] = useState(curtidasIniciais);
+
+        function salvarPost() {
+            setSavePost(prev => prev === 'bookmark-outline' ? 'bookmark' : 'bookmark-outline');
+        }
+
+        function curtir() {
+            if (like === 'heart-outline') {
+                setLike('heart');
+                setCurtidas(prev => prev + 1);
+            } else {
+                setLike('heart-outline');
+                setCurtidas(prev => prev - 1);
+            }
+        }
+
+        function curtirPost() {
+            if (like === 'heart-outline') {
+                setLike('heart');
+                setCurtidas(prev => prev + 1);
+            }
+        }
+
         return (
-            <>
+            <div className="post-item">
                 <div className="topo">
                     <div className="usuario">
                         <img src={imgUsuario} alt={usuario} />
@@ -19,44 +46,43 @@ export default function Post() {
                 </div>
 
                 <div className="conteudo">
-                    <img src={imagem} alt={usuario} />
+                    <img onClick={curtirPost} src={imagem} alt={usuario} />
                 </div>
 
                 <div className="fundo">
                     <div className="acoes">
                         <div>
-                            <ion-icon name="heart-outline"></ion-icon>
+                            <ion-icon onClick={curtir} name={like}></ion-icon>
                             <ion-icon name="chatbubble-outline"></ion-icon>
                             <ion-icon name="paper-plane-outline"></ion-icon>
                         </div>
                         <div>
-                            <ion-icon name="bookmark-outline"></ion-icon>
+                            <ion-icon onClick={salvarPost} name={savePost}></ion-icon>
                         </div>
                     </div>
 
                     <div className="curtidas">
                         <img src="assets/img/respondeai.svg" alt="respondeai" />
                         <div className="texto">
-                            Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
+                            Curtido por <strong>respondeai</strong> e <strong>outras {curtidas} pessoas</strong>
                         </div>
                     </div>
                 </div>
-            </>
-        )
+            </div>
+        );
     }
-
-
 
     return (
         <div className="post">
-            {posts.map((item) => (
+            {posts.map((item, index) => (
                 <PostItem
-                    key={item.usuario}
+                    key={index}
                     imagem={item.imagem}
                     imgUsuario={item.imgUsuario}
                     usuario={item.usuario}
+                    curtidasIniciais={item.curtidas}
                 />
             ))}
         </div>
-    )
+    );
 }
--- src/components/User.js
+++ src/components/User.js
@@ -1,13 +1,36 @@
+import { useState } from "react";
+
 export default function User() {
+    const [user, setUser] = useState("catanacomics");
+    const [imagem, setImagem] = useState("assets/img/catanacomics.svg");
+
+    function mudarUser() {
+        const nome = prompt("Qual o seu usuário?");
+        if (!nome) {
+            setUser("catanacomics");
+        } else {
+            setUser(nome);
+        }
+    }
+
+    function mudarImagem() {
+        const imagemUser = prompt("Qual o link da nova imagem?");
+        if (!imagemUser) {
+            setImagem("assets/img/catanacomics.svg");
+        } else {
+            setImagem(imagemUser);
+        }
+    }
+
     return (
         <div className="usuario">
-            <img src="assets/img/catanacomics.svg" alt="imagem de perfil" />
+            <img src={imagem} alt="imagem de perfil" onClick={mudarImagem} />
             <div className="texto">
                 <span>
-                    <strong>catanacomics</strong>
-                    <ion-icon name="pencil"></ion-icon>
+                    <strong>{user}</strong>
+                    <ion-icon onClick={mudarUser} name="pencil"></ion-icon>
                 </span>
             </div>
         </div>
-    )
+    );
 }
